4d3990074d81591d642169953b91f0c44516aa39
Harish Mathanan <harish.mathanan@hotmail.com>
2018-10-26 14:35:05 +0800

    Add: Routing for viewving the different examples.

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
 import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
+import { Route, Link } from 'react-router-dom';
 
 import ContactCard from './components/contactCard';
 import DocumentList from './components/documentList';
@@ -11,12 +12,26 @@ const App = (): JSX.Element => {
   return (
     <Fabric>
       <div>
-        <h1 className="ms-font-xxl">MS UI Fabric</h1>
+        <h1 className="ms-font-xxl">
+          <Link to="/">MS UI Fabric</Link>
+        </h1>
+      </div>
+
+      <div>
+        <Link to="/contact-card">Contact Card</Link>
+        &nbsp; | &nbsp;
+        <Link to="/document-list">Document List</Link>
       </div>
 
       <div className="ms-font-m" style={{ marginTop: 32 }}>
-        <ContactCard />
-        <DocumentList />
+        <Route
+          path="/"
+          exact={true}
+          render={() => <h3>Click on a sample to view.</h3>}
+        />
+
+        <Route path="/contact-card" component={ContactCard} />
+        <Route path="/document-list" component={DocumentList} />
       </div>
     </Fabric>
   );
